daf0939fa43b9f6a9de53f842af9a2240ddee491
Prathamesh Marathe <prath1611@gmail.com>
2020-08-25 08:52:08 -0400

    Wire up Login Form to API
    Connects the login form button to the authentication API (/token. Gets back the bearer token or exception if failed./

--- DesktopUI/Bootstrapper.cs
+++ DesktopUI/Bootstrapper.cs
@@ -1,4 +1,5 @@
 ﻿using Caliburn.Micro;
+using DesktopUI.Helpers;
 using DesktopUI.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -6,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace DesktopUI
 {
@@ -15,13 +17,19 @@ namespace DesktopUI
         public Bootstrapper()
         {
             Initialize();
+            ConventionManager.AddElementConvention<PasswordBox>(
+            PasswordBoxHelper.BoundPasswordProperty,
+            "Password",
+            "PasswordChanged");
         }
         protected override void Configure()
         {
             _container.Instance(_container);
             _container.
                 Singleton<IWindowManager, WindowManager>()
-                .Singleton<IEventAggregator, EventAggregator>();
+                .Singleton<IEventAggregator, EventAggregator>()
+                .Singleton<IAPIHelper,APIHelper>();
+                
 
             GetType().Assembly.GetTypes()
                 .Where(type => type.IsClass)
